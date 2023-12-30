707c049b41b63ee3aca46f4708f89ca12c51f125
Ankit Kumar Jha <ankitjha2701@gmail.com>
2023-12-30 16:39:40 +0530

    Create deleteANode.java

---
+++ deleteANode.java
@@ -0,0 +1,71 @@
+public class file7 {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    static Node insert(Node root,int val){
+        if(root==null){
+            root=new Node(val);
+            return root;
+        }
+        if(root.data>val){
+            root.left=insert(root.left,val);
+        }
+        else{
+            root.right=insert(root.right,val);
+        }
+        return root;
+    }
+    static Node delete(Node root,int val){
+        if(root.data>val){
+            root.left=delete(root.left,val);
+        }
+        else if(root.data<val){
+            root.right=delete(root.right,val);
+        }
+        else{
+            if(root.left==null && root.right==null){
+                return null;
+            }
+            if(root.left==null){
+                return root.right;
+            }
+            else if(root.right==null){
+                return root.left;
+            }
+            Node IS=findInOrderSuccessor(root.right);
+            root.data=IS.data;
+            root.right=delete(root.right,IS.data);
+        }
+        return root;
+    }
+    static Node findInOrderSuccessor(Node root){
+        while(root.left!=null){
+            root=root.left;
+        }
+        return root;
+    }
+    static void inOrder(Node root){
+        if(root==null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+    }
+    public static void main(String[] args) {
+        int values[]={8,5,3,1,4,6,10,11,14};
+        Node root=null;
+        for(int i=0;i<values.length;i++){
+            root=insert(root,values[i]);
+        }
+        root=delete(root,1);
+        inOrder(root);
+    }
+}
